01f2dd508e0eae2db1be0f9d2802a8c925b7fd07
cdanm9 <141808055+cdanm9@users.noreply.github.com>
2026-02-20 08:42:50 +0000

    Refactor light creation in World component to use RectAreaLight
    - Replaced DirectionalLight, PointLight, and SpotLight with RectAreaLight.
    - Updated light position for RectAreaLight.
    - Added commented-out code for alternative light types and configurations.
    - Included initialization for RectAreaLightUniformsLib.

--- threejsxp/threestart/src/World/components/lights.js
+++ threejsxp/threestart/src/World/components/lights.js
@@ -1,7 +1,21 @@
 import * as THREE from 'three';
+import { RectAreaLightUniformsLib } from 'three/examples/jsm/lights/RectAreaLightUniformsLib.js';
 function createLights(){
-    const light=new THREE.DirectionalLight('yellow',8);
-    light.position.set(10,10,10)
+    // const light=new THREE.DirectionalLight('white',8);/
+    // const light=new THREE.PointLight('white',8);
+    // const light=new THREE.SpotLight('white',8);
+    // light.position.set(10,10,10) //DirectionalLight
+    // light.position.set(0.1,0.1,0.1) //SpotLight
+    // light.position.set(0.1,0.1,0.1)//PointLight
+    // light.castShadow = true;
+
+    // RectAreaLightUniformsLib.init(); // only relevant for WebGLRenderer
+    const light = new THREE.RectAreaLight('white', 8);
+    light.position.set( 1, 1, 2.5 );//RectAreaLight
+    // light.lookAt( 0, 0, 0 );
+
+    // RectAreaLightUniformsLib.init()
+    // const intensity=8
     return light;
 }
 export{createLights}
